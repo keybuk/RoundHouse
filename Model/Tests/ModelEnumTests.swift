00744956c5fe9523eca722bd621dcca2f4aaf912
Scott James Remnant <scott@netsplit.com>
2021-07-01 17:18:20 -1000

    Test enum values are stable
    Since raw values are stored in the database, we need to not accidentally break the rawValue conversion of the enums. Reordering changes like I did for Model.Classification should make test cases fail, to alert that a migration needs to be done.

--- Model/Tests/ModelEnumTests.swift
+++ Model/Tests/ModelEnumTests.swift
@@ -42,7 +42,7 @@ class ModelEnumTests: XCTestCase {
     ///
     /// This test ensures stability of the database content.
     func testClassificationFromRawValue() throws {
-        for rawValue in (1 as Int16)...(5 as Int16) {
+        for rawValue in (1 as Int16)...(8 as Int16) {
             switch (rawValue, Model.Classification(rawValue: rawValue)) {
             case (1, .dieselElectricLocomotive): break
             case (2, .steamLocomotive): break
@@ -53,7 +53,7 @@ class ModelEnumTests: XCTestCase {
             case (7, .noPrototype): break
             case (8, .vehicle): break
             default:
-                XCTFail("Classification has incorrect value")
+                XCTFail("Classification has incorrect value for rawValue \(rawValue)")
             }
         }
     }
@@ -63,6 +63,70 @@ class ModelEnumTests: XCTestCase {
         XCTAssertNil(Model.Classification(rawValue: 0), "Classification has incorrect value")
     }
     
+    // MARK: Model.Era
+
+    /// Check that the raw values of the enums are as expected.
+    ///
+    /// This test ensures stability of the database content.
+    func testEraRawValue() throws {
+        for era in Model.Era.allCases {
+            switch era {
+            case .pioneering:
+                XCTAssertEqual(era.rawValue, 1, "rawValue has incorrect value")
+            case .preGrouping:
+                XCTAssertEqual(era.rawValue, 2, "rawValue has incorrect value")
+            case .theBigFour:
+                XCTAssertEqual(era.rawValue, 3, "rawValue has incorrect value")
+            case .brSteamEarlyCrest:
+                XCTAssertEqual(era.rawValue, 4, "rawValue has incorrect value")
+            case .brSteamLateCrest:
+                XCTAssertEqual(era.rawValue, 5, "rawValue has incorrect value")
+            case .brCorporateBluePreTOPS:
+                XCTAssertEqual(era.rawValue, 6, "rawValue has incorrect value")
+            case .brCorporateBluePostTOPS:
+                XCTAssertEqual(era.rawValue, 7, "rawValue has incorrect value")
+            case .brSectorisation:
+                XCTAssertEqual(era.rawValue, 8, "rawValue has incorrect value")
+            case .initialPrivatisation:
+                XCTAssertEqual(era.rawValue, 9, "rawValue has incorrect value")
+            case .rebuildingOfTheRailways:
+                XCTAssertEqual(era.rawValue, 10, "rawValue has incorrect value")
+            case .currentEra:
+                XCTAssertEqual(era.rawValue, 11, "rawValue has incorrect value")
+            default:
+                XCTFail("Era \(era) not expected")
+            }
+        }
+    }
+    
+    /// Check that the enum can be initialized from a raw value.
+    ///
+    /// This test ensures stability of the database content.
+    func testEraFromRawValue() throws {
+        for rawValue in (1 as Int16)...(11 as Int16) {
+            switch (rawValue, Model.Era(rawValue: rawValue)) {
+            case (1, .pioneering?): break
+            case (2, .preGrouping?): break
+            case (3, .theBigFour?): break
+            case (4, .brSteamEarlyCrest?): break
+            case (5, .brSteamLateCrest?): break
+            case (6, .brCorporateBluePreTOPS?): break
+            case (7, .brCorporateBluePostTOPS?): break
+            case (8, .brSectorisation?): break
+            case (9, .initialPrivatisation?): break
+            case (10, .rebuildingOfTheRailways?): break
+            case (11, .currentEra?): break
+            default:
+                XCTFail("Era has incorrect value for rawValue \(rawValue)")
+            }
+        }
+    }
+    
+    /// Check that enum is nil when initialized with zero.
+    func testEraFromZero() throws {
+        XCTAssertNil(Model.Era(rawValue: 0), "Era has incorrect value")
+    }
+
     // MARK: Model.Disposition
 
     /// Check that the raw values of the enums are as expected.
@@ -85,13 +149,13 @@ class ModelEnumTests: XCTestCase {
     ///
     /// This test ensures stability of the database content.
     func testDispositionFromRawValue() throws {
-        for rawValue in (1 as Int16)...(5 as Int16) {
+        for rawValue in (1 as Int16)...(3 as Int16) {
             switch (rawValue, Model.Disposition(rawValue: rawValue)) {
             case (1, .normal): break
             case (2, .collectorItem): break
             case (3, .spareParts): break
             default:
-                XCTFail("Disposition has incorrect value")
+                XCTFail("Disposition has incorrect value for rawValue \(rawValue)")
             }
         }
     }
