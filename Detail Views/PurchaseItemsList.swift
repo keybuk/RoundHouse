5648026777fad72ff521c49ce5806d4b4bfac02e
Scott James Remnant <scott@netsplit.com>
2021-08-30 12:43:36 -0700

    PurchaseItemsList - start off detail view code with the models + accessories for a purchase

---
+++ Detail Views/PurchaseItemsList.swift
@@ -0,0 +1,61 @@
+//
+//  PurchaseItemsList.swift
+//  PurchaseItemsList
+//
+//  Created by Scott James Remnant on 8/30/21.
+//
+
+import SwiftUI
+
+struct PurchaseItemsList: View {
+    @ObservedObject var purchase: Purchase
+
+    @FetchRequest
+    var models: FetchedResults<Model>
+
+    @FetchRequest
+    var accessories: FetchedResults<Accessory>
+
+    init(purchase: Purchase) {
+        self.purchase = purchase
+        _models = FetchRequest(
+            sortDescriptors: [
+                SortDescriptor(\Model.index)
+            ],
+            predicate: NSPredicate(format: "purchase == %@", purchase),
+            animation: .default)
+        _accessories = FetchRequest(
+            sortDescriptors: [
+                SortDescriptor(\Accessory.index)
+            ],
+            predicate: NSPredicate(format: "purchase == %@", purchase),
+            animation: .default)
+    }
+
+    var body: some View {
+        List {
+            if models.count > 0 {
+                Section(header: accessories.count > 0 ? Text("Models") : nil) {
+                    ForEach(models) { model in
+                        ModelCell(model: model, showClass: true)
+                    }
+                }
+            }
+            if accessories.count > 0 {
+                Section(header: models.count > 0 ? Text("Accessories") : nil) {
+                    ForEach(accessories) { accessory in
+                        AccessoryCell(accessory: accessory)
+                    }
+                }
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct PurchaseItemsList_Previews: PreviewProvider {
+    static var previews: some View {
+        PurchaseItemsList(purchase: PreviewData.shared.purchases["R1234M"]!)
+            .environment(\.managedObjectContext, PreviewData.shared.viewContext)
+    }
+}
